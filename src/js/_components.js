a59c2378a2c8956f440012bd97a1da45c719ffb8
gtasa3box <gtasa3box@gmail.com>
2022-03-25 14:40:10 +0300

    f

--- src/js/_components.js
+++ src/js/_components.js
@@ -3,3 +3,17 @@ $('.questions__tab-title').click(function(){
     $(this).toggleClass('active');
     $($(this).data("target")).stop().slideToggle();
 });
+
+//input-tel
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    initialCountry: "ru",
+    utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+document.querySelector('#phone').addEventListener('input', 
+    function(e){
+    this.value = this.value.replace(/[^\d.]/g, '');
+    }
+)
